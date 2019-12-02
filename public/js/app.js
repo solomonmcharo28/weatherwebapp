e365fc06add186b82095e3c09371d1bb0546444c
Solomon <Mcharo@Solomons-MacBook-Pro.local>
2019-12-02 02:08:52 -0600

    My WebApp

--- public/js/app.js
+++ public/js/app.js
@@ -17,8 +17,9 @@ weatherForm.addEventListener('submit', (e)=>{
         response.json().then((data) =>{
             if(data.error){
             messageOne.textContent = 'Location Not Found'
-            messageTwo.textContent = ''
             messageThree.textContent = ''
+            messageTwo.textContent = ''
+            
             }
             else{
             
